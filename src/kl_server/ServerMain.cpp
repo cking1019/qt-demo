ffa6e117824e44118c84e8307e019cb092549272
Demo <183799815@qq.com>
2024-09-26 22:51:03 +0800

    修改server目录

--- src/kl_server/ServerMain.cpp
+++ src/kl_server/ServerMain.cpp
@@ -4,63 +4,62 @@
 #include <QDebug>
 #include <QTimer>
 
-QTcpServer* server = new QTcpServer();
-QList<QTcpSocket*> clientSockets;
+QTcpServer* tcpServer = new QTcpServer();
+QList<QTcpSocket*> tcpSocks;
 
 // 定时发送数据
 void sendData2RTM() {
     QTimer* pSend2clientTimer = new QTimer();
     QObject::connect(pSend2clientTimer, &QTimer::timeout, [=](){
-        for(auto c : clientSockets) {
-            c->write("Hello! I am VOI server.");
+        for(auto c : tcpSocks) {
+            c->write("Hello! I am VOI tcpServer.");
         }
     });
     pSend2clientTimer->start(1000);
 }
 
-// 读取数据
-void readDataFromClient(QTcpSocket* clientSocket)
-{
-    QByteArray data = clientSocket->readAll();
-    qDebug() << "Received message from client: " + data.toHex();
-}
-
-// 断开连接
-void clientDisconnected(QTcpSocket* clientSocket)
-{
-    clientSockets.removeOne(clientSocket);
-    clientSocket->deleteLater();
-    qDebug() << "Client disconnected: " << clientSocket->peerAddress().toString();
-}
-
-
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    if (!server->listen(QHostAddress::AnyIPv4, 1234))
+    if (!tcpServer->listen(QHostAddress::AnyIPv4, 1234))
     {
         qDebug() << "Server could not be started!";
-        return -1;
+        return 0;
     }
+    qDebug() << "Server is be started and is listening port 1234";
 
-    // 监听新连接
-    QObject::connect(server, &QTcpServer::newConnection, [&](){
-        qDebug() << "This is  a new connection from client:";
-        QTcpSocket* clientSocket = server->nextPendingConnection();
-        if (clientSocket != nullptr) {
-            clientSockets.append(clientSocket);
-            QObject::connect(clientSocket, &QTcpSocket::readyRead, [=](){
-                readDataFromClient(clientSocket);
-            });
-            QObject::connect(clientSocket, &QTcpSocket::disconnected, [=](){
-                clientDisconnected(clientSocket);
-            });
-        }
+    QObject::connect(tcpServer, &QTcpServer::newConnection, [=](){
+        QTcpSocket* tcpSock = tcpServer->nextPendingConnection();
+        if(tcpSock == nullptr) return;
+        QString addr = tcpSock->peerAddress().toString();
+        quint16 port = tcpSock->peerPort();
+        qDebug() << QString("connected from %1:%2").arg(addr).arg(port);
+
+        QObject::connect(tcpSock, &QTcpSocket::readyRead, [=](){
+            QByteArray buf = tcpSock->readAll();
+            qDebug() << QString("recv from %1:%2:").arg(addr).arg(port) 
+                     << buf.toHex();
+        });
+        QObject::connect(tcpSock, &QTcpSocket::connected, [=](){
+            qDebug() << QString("connected from %1:%2").arg(addr).arg(port);
+        });
+        QObject::connect(tcpSock, &QTcpSocket::disconnected, [=](){
+            qDebug() << QString("disconnected from %1:%2").arg(addr).arg(port);
+            tcpSocks.removeOne(tcpSock);
+            tcpSock->deleteLater();
+        });
+        tcpSocks.append(tcpSock);
     });
     
-    sendData2RTM();
+    QTimer* qTimer = new QTimer();
+    QObject::connect(qTimer, &QTimer::timeout, [=](){
+        for(auto c : tcpSocks) {
+            c->write("Hello! I am VOI tcpServer.");
+        }
+    });
+    qTimer->start(1000);
 
-    qDebug() << "Server is be started and is listening port 1234";
+    
     return app.exec();
 }
