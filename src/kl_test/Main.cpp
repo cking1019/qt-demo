80afd1d7a333bb03655cba9dc108245bf8ea7e7e
Demo <183799815@qq.com>
2024-09-27 15:11:59 +0800

    代码修改

--- src/kl_test/Main.cpp
+++ src/kl_test/Main.cpp
@@ -1,20 +1,74 @@
-#include <QCoreApplication>
-#include <QThread>
 #include <QDebug>
-#include "WorkerTcp.hpp"
-#include "WorkerUdp.hpp"
+#include <QDate>
 #include <QTcpSocket>
+#include <QObject>
+#include <QTimer>
+#include <QHostAddress>
+#include <QCoreApplication>
 
-int val = 10;
+#pragma pack(push, 1)
+struct TimeC {
+    uint8_t id;
+    uint64_t s_time;
+};
+#pragma pack(pop)
 
-int main(int argc, char *argv[])
-{
-    
+int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
-    int val = 100;
-    ::val += 10;
-    qDebug() << ::val;
-    qDebug() << val;
 
+    QTcpSocket tcpSock;
+    QTimer qtimer1;
+    QObject::connect(&qtimer1, &QTimer::timeout, [&](){
+        while (!tcpSock.waitForConnected()) {
+            qDebug() << "Attempting to connect...";
+            // tcpSock.connectToHost(QHostAddress("192.168.1.77"), 4444);
+            tcpSock.connectToHost(QHostAddress("127.0.0.1"), 17001);
+        }
+    });
+    QObject::connect(&tcpSock, &QTcpSocket::connected, [&](){
+        qDebug() << "Connected to host!";
+        qtimer1.stop();
+    });
+    QObject::connect(&tcpSock, &QTcpSocket::disconnected, [&](){
+        qDebug() << "Disconnected from server!";
+        qtimer1.start(1000);
+    });
+    QObject::connect(&tcpSock, &QTcpSocket::readyRead, [&](){
+        QByteArray buf;
+        if(tcpSock.bytesAvailable() > 0) {
+            buf = tcpSock.read(tcpSock.bytesAvailable());
+        }
+        qDebug() << "recv:" << buf.toHex();
+    });
+
+    QTimer qtimer2;
+    QObject::connect(&qtimer2, &QTimer::timeout, [&](){
+        QByteArray buf;
+        buf.append(QByteArray::fromHex("EEEEEEEE 03022b00 0000e807 09190f31 31720010 00040000 00000000 0000"));
+        // eeeeeeee 03022b00 0000e807 09190f31 
+        // 31720010 00040000 00000000 00 00 cdeaf36600000000 00aa aaaaaa
+        // 1,727,261,389
+        TimeC t;
+        t.id = 0;
+        // t.s_time = time(NULL);
+        t.s_time = QDateTime::currentDateTime().toSecsSinceEpoch();
+        qDebug() << t.s_time << "  " << buf.length();
+        buf.resize(buf.length() + 8);
+        qDebug() << buf.length();
+        memcpy(buf.data() + 30, &t.s_time, 8);
+        // QByteArray buf_time;
+        // buf_time.resize(8);
+        // memcpy(buf_time.data(), &t.s_time, 8);
+        // buf.append(buf_time);
+        buf.append(QByteArray::fromHex("00 AAAAAAAA"));
+
+        tcpSock.write(buf);
+        tcpSock.flush();
+        qDebug() << "send:" << buf.toHex()
+                 << "size:" << buf.size();
+    });
+
+    qtimer1.start(1000);
+    qtimer2.start(1000);
     return app.exec();
 }
